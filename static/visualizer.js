3bd981d4cd88cbf093b2651bad7e01fe1d1dd719
ssgssg2fan <realbongu1004@gmail.com>
2026-01-13 14:42:18 +0900

    Update visualizer.js

--- static/visualizer.js
+++ static/visualizer.js
@@ -105,9 +105,9 @@ window.onload = () => {
 
             // Chess.js from/to는 e2, e4 같은 문자열
             // Chess.js 좌표 -> 화면 좌표
-            const fromFile = lastMove.from.charCodeAt(0)-'a'.charCodeAt(0);
+            const fromFile = 7 - lastMove.from.charCodeAt(0)+'a'.charCodeAt(0);
             const fromRank = parseInt(lastMove.from[1])-1;
-            const toFile = lastMove.to.charCodeAt(0)-'a'.charCodeAt(0);
+            const toFile = 7 - lastMove.to.charCodeAt(0)+'a'.charCodeAt(0);
             const toRank = parseInt(lastMove.to[1])-1;
 
             if ((f===fromFile && r===fromRank) || (f===toFile && r===toRank)) {
